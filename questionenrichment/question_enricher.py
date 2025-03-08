f40bee55540f1a1fe73cc03d228255587f085061
Mark Szabo <96.mark.szabo@gmail.com>
2025-03-08 23:37:19 +0100

    Added the option to retrieve the 3 most relevant questions.

--- questionenrichment/question_enricher.py
+++ questionenrichment/question_enricher.py
@@ -1,7 +1,8 @@
 
 
-from dbconnector.es_functions import find_card_names_in_string
-
+from dbconnector.es_functions import find_card_names_in_string, get_closest_vectors
+import os
+from dotenv import load_dotenv
 
 def get_present_card_info(question):
     potentional_present_card_names = find_card_names_in_string(question)
@@ -20,5 +21,16 @@ def get_present_card_info(question):
             reduced_cardnames.append(obj)
     print(reduced_cardnames)
       
-    
+def get_relevant_questions(user_input):
+    releveant_questions = []
+    load_dotenv()
+    question_index = os.getenv("ES_QUESTION_INDEX")
+    results = get_closest_vectors(user_input,question_index)
+    for idx, result in enumerate(results):
+        content = result['source'].get('content', 'No content available')
+        releveant_questions.append(content)    
+    return releveant_questions
+
+def get_full_prompt(present_cards,relevant_questions,relevant_rules,user_input):
+    return "Kappa"
     
